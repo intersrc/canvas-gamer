506333d8b94618f59a42fa67eab91dd3f97b4098
吴语 <wuyu@lingxi360.com>
2017-02-21 15:10:34 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var path = require('path')
+
+module.exports = {
+  entry: {
+    index: ['./src/index'],
+    examples: ['babel-polyfill', './examples/index.js']
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist/',
+    filename: process.env.NODE_ENV === 'production' ? '[name].min.js' : '[name].js',
+    library: 'canvasGamer',
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    }, {
+      test: /\.vue$/,
+      loader: 'vue'
+    }, {
+      test: /\.png$/,
+      loader: 'url-loader',
+      options: { mimetype: 'image/png' }
+    }]
+  },
+  devtool: '#source-map',
+  devServer: {
+    port: 8889,
+    host: '0.0.0.0'
+  },
+  plugins: []
+}
